643bb429baea6876c5f631cf33283e6a11733d41
Queen103 <128125739+Queen103@users.noreply.github.com>
2024-12-17 09:57:51 +0700

    cập nhật detail

--- src/app/components/CardDetail.tsx
+++ src/app/components/CardDetail.tsx
@@ -38,17 +38,21 @@ const CardDetail: React.FC<CardDetailProps> = ({ machine, isDarkMode, isFullScre
                 : "border-notConnect";
 
     return (
-        <div className={`text-center border-4 rounded-lg transition-transform hover:scale-[102%] ${borderColor} ${isDisabled ? 'opacity-40 bg-gray-400' : isDarkMode ? 'bg-secondary' : 'bg-bg-light'}`}>
+        <div className={`text-center border-4 rounded-lg h-5vh transition-transform hover:scale-[102%] ${borderColor} ${isDisabled ? 'opacity-40 bg-gray-400' : isDarkMode ? 'bg-secondary' : 'bg-bg-light'}`}>
             <h3 className={`text-text-dark ${isFullScreen ? "py-3 text-3xl" : "py-2 text-2xl"} rounded-t-sm font-semibold justify-center ${isDisabled ? 'bg-gray-500' : bgColor} ${isFullScreen ? "mb-4" : "mb-2"}`}>
                 {machine.name}
             </h3>
-            <div className={`mb-3 grid grid-cols-2 gap-y-2 p-1 item-center px-3 ${isDisabled ? 'text-gray-700' : ''}`}>
+            <div className={`mb-3 grid grid-cols-2 gap-y-2 p-1 item-between px-3 ${isDisabled ? 'text-gray-700' : ''}`}>
                 <strong className={`text-start text-xl ${isDisabled ? 'text-gray-700' : isDarkMode ? 'text-text-dark' : 'text-text-light'}`}>Mục Tiêu Ngày</strong>
                 <span className={`text-3xl text-end ${isDisabled ? 'text-gray-700' : isDarkMode ? 'text-text-dark' : 'text-text-light'}`}>{machine.target}</span>
                 <strong className={`text-start text-xl ${isDisabled ? 'text-gray-700' : isDarkMode ? 'text-text-dark' : 'text-text-light'}`}>Mục Tiêu Giờ</strong>
                 <span className={`text-3xl text-end ${isDisabled ? 'text-gray-700' : isDarkMode ? 'text-text-dark' : 'text-text-light'}`}>{machine.mtg}</span>
                 <strong className={`text-start text-xl ${isDisabled ? 'text-gray-700' : isDarkMode ? 'text-text-dark' : 'text-text-light'}`}>Thực Hiện</strong>
                 <span className={`text-3xl text-end ${isDisabled ? 'text-gray-700' : isDarkMode ? 'text-text-dark' : 'text-text-light'}`}>{machine.actual}</span>
+                <strong className={`text-start text-xl ${isDisabled ? 'text-gray-700' : isDarkMode ? 'text-text-dark' : 'text-text-light'}`}>Chênh Lệch</strong>
+                <span className={`text-3xl text-end ${(machine.actual - machine.mtg) > 0 ? 'text-green-500' : 'text-red-500'} `}>
+                    {(machine.actual - machine.mtg).toFixed(0)}
+                </span>
                 <strong className={`text-start text-xl ${isDisabled ? 'text-gray-700' : isDarkMode ? 'text-text-dark' : 'text-text-light'}`}>Hiệu Suất (%)</strong>
                 <span className={`text-3xl text-end ${isDisabled ? 'text-gray-700' : isDarkMode ? 'text-text-dark' : 'text-text-light'}`}>
                     {(machine.mtg <= 0 || machine.actual < 0) ? "-" : ((machine.actual / machine.mtg) * 100).toFixed(1)}
